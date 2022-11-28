4f134f36680e22723c994265b689a2cad7d3d949
Arina Zaporozhets <80780314+Arina-Zap@users.noreply.github.com>
2022-11-28 11:27:38 +0100

    lab5 was added

---
+++ lab5/Pages/Page.cs
@@ -0,0 +1,20 @@
+﻿namespace lab5;
+
+using lab5.Interfaces;
+
+public abstract class Page
+{
+    protected IRenderer _renderer;
+
+    public IRenderer Renderer
+    {
+        set { _renderer = value; }
+    }
+
+    public Page(IRenderer renderer)
+    {
+        _renderer = renderer;
+    }
+
+    public abstract void Render();
+}
